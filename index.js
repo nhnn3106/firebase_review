049e3bf287152f8caa59c342438f9a564fcce952
nhnn3106 <120386489+nhnn3106@users.noreply.github.com>
2024-07-13 23:25:30 +0700

    onValue learn

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js";
 
+import { getDatabase, ref, push, onValue } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js";
 
 
 const appSettings = {
@@ -7,13 +8,29 @@ const appSettings = {
 }
 
 const app = initializeApp(appSettings);
-
+const database = getDatabase(app)
+const shoppingListInDB = ref(database, "shoppingList")
 console.log(app);
 
 const inputFieldEl = document.getElementById("input-field");
 const addButtonEl = document.getElementById("add-button");
-
+const shoppingListEl = document.getElementById("shopping-list");
 addButtonEl.addEventListener("click", () => {
     let inputValue = inputFieldEl.value;
-    console.log(inputValue);
+    push(shoppingListInDB, inputValue);
+    clearInputField();
+    appendItemValueToShoppingListEl(inputValue)
+    
+    console.log(`${inputValue} added to database`);
 });
+
+onValue(shoppingListInDB, function(snaphot) {
+    console.log(snaphot.val());
+});
+
+function clearInputField() {
+    inputFieldEl.value = "";
+}
+function appendItemValueToShoppingListEl(itemValue) {
+    shoppingListEl.innerHTML += "<li>" + itemValue + "</li>";
+}
